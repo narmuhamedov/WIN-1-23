22b57a6be7fd04e467b753208cd5818c8bfd3616
rzpy1997 <kingradomir@gmail.com>
2023-12-04 08:56:58 +0600

    search

--- main_page/urls.py
+++ main_page/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import film_list_view, film_list_detail_view, create_review_film
+from .views import main_list_view, film_list_detail_view, create_review_film, premieres_view, SearchView
 
 urlpatterns = [
-    path('film_list/', film_list_view, name='film_list'),
+    path('', main_list_view, name='film_list'),
+    path('premieres/', premieres_view, name='premieres'),
     path('film_list/<int:id>/', film_list_detail_view),
     path('create-review/', create_review_film),
+    path('search/', SearchView.as_view(), name='search')
 ]
--- main_page/views.py
+++ main_page/views.py
@@ -2,19 +2,23 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import FilmListModel, Afisha, Slider
 from . import forms
 from django.urls import reverse
+from django.views import generic
 
 
 # Не полная информация
-def film_list_view(request):
+def main_list_view(request):
     if request.method == "GET":
-        film_list = FilmListModel.objects.all()
         afisha_list = Afisha.objects.all()
         slider_list = Slider.objects.all()
         return render(request, template_name='films/index.html', context={
-            'film_list': film_list,
             'afisha_list': afisha_list,
             'slider_list': slider_list
         })
+
+
+def premieres_view(request):
+    film_list = FilmListModel.objects.all()
+    return render(request, template_name='premieres/premieres.html', context={'film_list': film_list, })
 
 
 # GET ID
@@ -34,3 +38,19 @@ def create_review_film(request):
     else:
         form = forms.FilmForm()
         return render(request, template_name='create_review.html', context={'form': form})
+
+
+# SEARCH
+
+class SearchView(generic.ListView):
+    template_name = 'premieres/premieres.html'
+    context_object_name = 'film_list'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return FilmListModel.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('id')
+        return context
